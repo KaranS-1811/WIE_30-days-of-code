ffb29ba6e00bda0a575613b68e74cd519c201b83
Karanbir Singh <74266295+KaranS-1811@users.noreply.github.com>
2022-04-26 20:00:37 +0530

    Karanbir_1st_day_code.py

---
+++ Python/Karanbir_1st_day_code.py
@@ -0,0 +1,3 @@
+#here were are going ot create simple code for array and string.
+a = "Hello, World!"   #Here we make a variable and assign some value to it in string.
+print(a)              #Here we are printing the sting onto the output window.
