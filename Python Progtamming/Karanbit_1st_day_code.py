5312215801c45885ea8fa144be5e94992a0c31cc
Karanbir Singh <74266295+KaranS-1811@users.noreply.github.com>
2022-04-26 20:00:04 +0530

    Delete Python Progtamming directory

--- Python Progtamming/Karanbit_1st_day_code.py
+++
@@ -1,3 +0,0 @@
-#here were are going ot create simple code for array and string.
-a = "Hello, World!"   #Here we make a variable and assign some value to it in string.
-print(a)              #Here we are printing the sting onto the output window.
